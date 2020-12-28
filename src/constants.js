ce178321317721be5474dfb4e0766afb3e7efa79
krlosMata <krlosmata@gmail.com>
2020-12-28 21:31:25 +0100

    add account-creation-auth function

--- src/constants.js
+++ src/constants.js
@@ -32,3 +32,4 @@ module.exports.maxNlevels = 48;
 module.exports.exitAx = "0x0000000000000000000000000000000000000000000000000000000000000000";
 module.exports.exitAy = "0x0000000000000000000000000000000000000000000000000000000000000000";
 module.exports.nullEthAddr = "0xffffffffffffffffffffffffffffffffffffffff";
+module.exports.createAccountMsg = "I authorize this babyjubjub key for hermez rollup account creation";
